3ec1c03265021a85637d29afec758f72389b5ea7
yfaywise <37440341+yfaywise@users.noreply.github.com>
2024-01-10 14:43:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
-
-print 'hello';
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:srvbdtest1.database.windows.net,1433; Database = bdtest2", "CloudSAe1f960f0", "Dabbad_17");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("insert into nombres values (10)");
+print("works");
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
 
 ?>
+
+
